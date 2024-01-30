0bca586155d8313d5e4f9bf01db26ee911950a12
Wonyoung Kang <onezerokang@gmail.com>
2024-01-30 19:34:37 +0900

    [#3] Member 테이블에 created_at, updated_at 필드 추가

--- src/main/java/com/onezerokang/springlab/member/domain/Member.java
+++ src/main/java/com/onezerokang/springlab/member/domain/Member.java
@@ -1,12 +1,13 @@
 package com.onezerokang.springlab.member.domain;
 
+import com.onezerokang.springlab.global.domain.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Member {
+public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
